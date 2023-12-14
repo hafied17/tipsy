dd0b7ec5f95ebbece207359b96cd16926836d189
hafied17 <hafiedmadrid6@gmail.com>
2023-12-14 14:47:47 +0700

    Initial Commit

---
+++ tipsy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tipsy
+//
+//  Created by Investo Medika Asia on 14/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
